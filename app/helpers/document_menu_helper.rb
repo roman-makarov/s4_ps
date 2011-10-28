dd6793a6f7842b648d8a8657952edf1959f714ea
Roman A. Makarov <roman@roman-laptop-job.site-job>
2011-10-28 13:21:57 +0400

    Commit

--- app/helpers/document_menu_helper.rb
+++ app/helpers/document_menu_helper.rb
@@ -4,7 +4,7 @@ module DocumentMenuHelper
     navigation.ul :html => { :id => :contextual_menu, :class => :tabbed_menu } do |ul|
       ul.li t(:header, :scope => [:shared, :senddocument]), list_senddocuments_path,             :senddocuments => :list
       #ul.li "Регистрационные карточки", licenses_organization_path,    :organizations => :licenses
-      ul.li t(:header, :scope => [:shared, :authority]), warrant_senddocuments_path,  :senddocuments => :warrant
+      ul.li t(:header, :scope => [:shared, :authority]), authorities_path,  :authorities
       ul.li t(:header_message, :scope => [:shared, :senddocument]), message_senddocuments_path,     :senddocuments => :message
     end
   end
