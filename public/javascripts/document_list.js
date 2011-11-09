ae42d97830ff3899149626a3967de8fbc93ac6a6
Victor Firsov <firsov.victor@yandex.ru>
2011-11-09 16:20:42 +0400

    js fix

--- public/javascripts/document_list.js
+++ public/javascripts/document_list.js
@@ -7,15 +7,21 @@ function do_reset()
     document.getElementById("documentfilter_by_sender").value = "";
   }
 
+function check_select_bytype()
+{
+  if ( document.getElementById("documentfilter_by_type").value == 13 )
+  {
+    document.getElementById("documentfilter_document_name").disabled=false;
+  }
+  else
+  {
+    document.getElementById("documentfilter_document_name").disabled=true;
+  }
+}
+
 document.getElementById("documentfilter_document_name").disabled=true;
 
 document.getElementById('documentfilter_by_type').onchange = function() {
-    if ( document.getElementById("documentfilter_by_type").value == 13 )
-    {
-      document.getElementById("documentfilter_document_name").disabled=false;
-    }
-    else
-    {
-      document.getElementById("documentfilter_document_name").disabled=true;
-    }
+    check_select_bytype();
 }
+check_select_bytype();
